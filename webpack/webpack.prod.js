0d8b11238f4aed228f8acfaffed2b5952e324cc2
Roberto Carlos Galicia Lopez <dev.robertgl@gmail.com>
2021-08-10 15:39:04 -0500

    configuracion basica webpack

---
+++ webpack/webpack.prod.js
@@ -0,0 +1,31 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { merge }            = require("webpack-merge");
+const common               = require("./webpack.common");
+
+/** @type {import('webpack').Configuration} */
+const prodConfig = 
+{
+    mode    : "production",
+    // devtool : "source-map",
+    module  : 
+    {
+        rules: 
+        [
+            {
+                test: /\.(css|scss|sass)$/,
+                use: 
+                [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ],
+            },
+        ],
+    },
+    plugins: 
+    [
+        new MiniCssExtractPlugin()
+    ],
+};
+
+module.exports = merge(common, prodConfig);
